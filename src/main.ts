95129a735979630c103d0bcc15cb8c4f2839cf27
Brady Lin <linbrady16877@gmail.com>
2024-11-04 01:46:24 -0800

    L16 Dialog

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,5 @@
-// todo
+const button = document.createElement("button");
+button.textContent = "Click me";
+button.onclick = () => alert("You clicked the button!");
+
+document.body.appendChild(button);
