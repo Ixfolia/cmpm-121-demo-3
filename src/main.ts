5dff2a2f1167e3fbde07740032c4942eea5a5dfd
Brady Lin <linbrady16877@gmail.com>
2024-11-04 01:44:29 -0800

    L16 Dialog

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-// todo
+document.addEventListener('DOMContentLoaded', () => {
+    const app = document.getElementById('app');
+    if (app) {
+        const button = document.createElement('button');
+        button.innerText = 'Click Me';
+        button.addEventListener('click', () => {
+            alert('you clicked the button!');
+        });
+        app.appendChild(button);
+    }
+});
