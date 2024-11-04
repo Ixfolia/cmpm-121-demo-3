fac809b832943692f32c83e3fb2eed2750913dc1
Brady Lin <linbrady16877@gmail.com>
2024-11-04 01:45:42 -0800

    Revert "L16 Dialog"
    This reverts commit 5dff2a2f1167e3fbde07740032c4942eea5a5dfd.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const app = document.getElementById('app');
-    if (app) {
-        const button = document.createElement('button');
-        button.innerText = 'Click Me';
-        button.addEventListener('click', () => {
-            alert('you clicked the button!');
-        });
-        app.appendChild(button);
-    }
-});
+// todo
